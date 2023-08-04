ea67679b2421e4c22972cd260da3d6399adb144d
tommaso galliena <tommaso.galliena01@universitadipavia.it>
2023-08-04 16:48:16 +0200

    Implemented function to find border point of the image to deform

--- utils/mask_deformation.py
+++ utils/mask_deformation.py
@@ -2,36 +2,50 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 import math
-img = cv.imread("C:/Users/tommy/Downloads/data0_x_stack_56.png")
+import random
 
-right_eye = (78,74)
-radius = 30
-power = 1.6 # >1.0 for expansion, <1.0 for shrinkage
 
-height, width, _ = img.shape
-map_y = np.zeros((height,width),dtype=np.float32)
-map_x = np.zeros((height,width),dtype=np.float32)
+def find_deformation_point(img):
+    laplacian_kernel = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
+    filtered_img = cv.filter2D(src=img, kernel=laplacian_kernel, ddepth=-1) 
+    borderPoints_list = []
+    for row in range(0,img.shape[0]):
+        for column in range(0,img.shape[1]):
+            if filtered_img[row][column]!=0:
+                borderPoints_list.append((column,row))
+    return random.choice(borderPoints_list)
 
-# create index map
-for i in range(height):
-    for j in range(width):
-        map_y[i][j]=i
-        map_x[i][j]=j
+def apply_deformation(img, point, radius=45, power=1.6):
+    height, width = img.shape
+    map_y = np.zeros((height,width),dtype=np.float32)
+    map_x = np.zeros((height,width),dtype=np.float32)
 
-# deform around the right eye
-for i in range (-radius, radius):
-    for j in range(-radius, radius):
-        if (i**2 + j**2 > radius ** 2):
-            continue
+    # create index map
+    for i in range(height):
+        for j in range(width):
+            map_y[i][j]=i
+            map_x[i][j]=j
 
-        if i > 0:
-            map_y[right_eye[1] + i][right_eye[0] + j] = right_eye[1] + (i/radius)**power * radius
-        if i < 0:
-            map_y[right_eye[1] + i][right_eye[0] + j] = right_eye[1] - (-i/radius)**power * radius
-        if j > 0:
-            map_x[right_eye[1] + i][right_eye[0] + j] = right_eye[0] + (j/radius)**power * radius
-        if j < 0:
-            map_x[right_eye[1] + i][right_eye[0] + j] = right_eye[0] - (-j/radius)**power * radius
+    # deform around the right eye
+    for i in range (-radius, radius):
+        for j in range(-radius, radius):
+            if (i**2 + j**2 > radius ** 2) or i < 0 or j < 0:
+                continue
 
-warped=cv.remap(img,map_x,map_y,cv.INTER_LINEAR)
-cv.imwrite('data0_x_stack_56_modified.jpg', warped)
+            if i > 0:
+                map_y[point[1] + i][point[0] + j] = point[1] + (i/radius)**power * radius
+            if i < 0:
+                map_y[point[1] + i][point[0] + j] = point[1] - (-i/radius)**power * radius
+            if j > 0:
+                map_x[point[1] + i][point[0] + j] = point[0] + (j/radius)**power * radius
+            if j < 0:
+                map_x[point[1] + i][point[0] + j] = point[0] - (-j/radius)**power * radius
+
+    warped=cv.remap(img,map_x,map_y,cv.INTER_LINEAR)
+    cv.imwrite('result_img.jpg', warped)
+    
+if __name__=="__main__":
+    img_ = cv.imread("C:/Users/tommy/Downloads/data0_z_stack_94.png")
+    gray_image = cv.cvtColor(img_, cv.COLOR_BGR2GRAY)
+    p = find_deformation_point(gray_image)
+    apply_deformation(gray_image, p)
